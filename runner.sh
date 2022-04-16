e718e88b4c0bc6c09076d5746f5a545eca8cf74e
Akito Mizukito <akito@evolution-x.org>
2022-04-16 22:23:22 +0900

    ci: Migrate to HTML
    Change-Id: Ib82e7eb2997f8f24f2a6e639764308e87d651e60
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- runner.sh
+++ runner.sh
@@ -8,7 +8,7 @@ COMMIT_SMALL_HASH="$(git rev-parse --short HEAD)"
 COMMIT_HASH="$(git rev-parse --verify HEAD)"
 
 function sendTG() {
-    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001539241326&parse_mode=Markdown"
+    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001539241326&parse_mode=HTML"
 }
 
 printf "\n\n***Evolution X CI***\n\n"
@@ -19,7 +19,7 @@ if [ -n "$PULL_REQUEST_NUMBER" ]; then
 else
     git checkout master  > /dev/null
     git pull origin master  > /dev/null
-    sendTG "**I am starting to build!** %0A**Commit Point:** [${COMMIT_SMALL_HASH}](https://github.com/Evolution-X-Devices/official_devices/commit/${COMMIT_HASH})"
+    sendTG "<b>I am starting to build!</b> %0A<b>Commit Point:</b> <a href='https://github.com/Evolution-X-Devices/official_devices/commit/${COMMIT_HASH}'>${COMMIT_SMALL_HASH}</a>"
 fi
 
 node json_tester.js
@@ -29,17 +29,17 @@ RESULT=$?
 if [ -n "$PULL_REQUEST_NUMBER" ]; then
 
     if [ "$RESULT" -eq 1 ]; then
-        sendTG "\`PR $PULL_REQUEST_NUMBER is failing checks. Maintainer is requested to check it\` %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\` %0A%0A[PR Link](https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER)"
+        sendTG "\`PR $PULL_REQUEST_NUMBER is failing checks. Maintainer is requested to check it\` %0A%0A<b>Failed File:</b> \`$(cat /tmp/failedfile)\` %0A%0A<a href='https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER'>PR Link</a>"
         exit 1
     else
-        sendTG "\`PR $PULL_REQUEST_NUMBER can be merged.\` %0A%0A${ADMINS} %0A%0A[PR Link](https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER)"
+        sendTG "\`PR $PULL_REQUEST_NUMBER can be merged.\` %0A%0A${ADMINS} %0A%0A<a href='https://github.com/Evolution-X-Devices/official_devices/pull/$PULL_REQUEST_NUMBER'>PR Link</a>"
         exit 0
     fi
 
 fi
 
 if [ "$RESULT" -eq 1 ]; then
-    sendTG "\`Ay, Someone have merged broken file! Please take a look ASAP!\` %0A%0A${ADMINS} %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\`"
+    sendTG "\`Someone have merged broken file! Please take a look ASAP!\` %0A%0A${ADMINS} %0A%0A<b>Failed File:</b> \`$(cat /tmp/failedfile)\`"
     exit 1
 fi
 
